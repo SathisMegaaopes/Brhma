196d0c30d891ea7f4cd85ae0cdc4a09286584047
Sathis kumar <sathiskumar.r@megaaopes.com>
2024-10-02 19:59:49 +0530

    api started message

--- routes/employeeOnboard/employeeOnboard.js
+++ routes/employeeOnboard/employeeOnboard.js
@@ -47,11 +47,74 @@ const transporter = nodemailer.createTransport({
 
 
 
+// firstname,
+// lastname,
+// dateOfBirth,
+// employeeNumber,
+// gender,
+// email,
+// mobileNumber,
+// phone,
+// bloodGroup,
+// dateOfJoining,
+// fathersName,
+// fathersOccupation,
+// countryOfOrigin,
+// nationality,
+// emergencyContactName,
+// emergencyContactNumber,
+// emergencyContactRelation,
+// spouseName,
+// physicallyChallenged,
+// education,
+// addressprofType,
+// permanentAddress,
+// permanentcity,
+// permanentPincode,
+// totalmonthlyctc,
+// totalyearlyctc,
+// bankname,
+// branchname,
+// ifsccode,
+// accountNumber,
+// beneficiarycode,
+// pfnumber,
+// pfjoindate
+
 
 router.post('/', (req, res) => {
 
-    console.error(req.body, 'This is the error');
-    res.sendStatus(200)
+    const { formData } = req.body;
+
+    const basicInfoQuery = ' INSERT INTO `employee_onboard` (`emp_id`, `first_name`, `last_name`, `dateofbirth`,' +
+        '`employee_number`, `gender`, `email`, `mobile_number`, `phone`, `blood_group`, `date_of_join`, `father_name`, `father_occupation`,' +
+        '`country_of_origin`, `nationality`, `emergency_contact_name`, `emergency_contact_number`, `emergency_contact_relation`, `spouse_name`,' +
+        ' `physically_challenged`, `education`, `address_prof_type`) VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+
+    // console.log(basicInfoQuery)
+
+    let data = [formData.firstname, formData.lastname, formData.dateOfBirth, formData.employeeNumber, formData.gender, formData.email]
+    data = [...data, formData.mobileNumber, formData.phone, formData.bloodGroup, formData.dateOfJoining, formData.fathersName, formData.fathersOccupation, formData.countryOfOrigin]
+    data = [...data, formData.nationality, formData.emergencyContactName, formData.emergencyContactNumber, formData.emergencyContactRelation]
+    data = [...data, formData.spouseName, formData.physicallyChallenged, formData.education, formData.addressprofType]
+
+
+    conn.query(basicInfoQuery, data, (err, rows) => {
+        if (err) {
+            console.log(err)
+            res.send('Something went wrong mapla , you have to face the consquences right .... ')
+        } else {
+            console.log('Success dude ehhhhhhhhhhh..................................')
+            res.send('Ok mapla , you are going good da , check the phpmyamin , post agi iruka illaya nu paru....')
+        }
+    })
+
+
+    // formData.firstname, formData.lastname, formData.dateOfBirth, formData.employeeNumber, formData.gender, formData.email
+    // formData.mobileNumber, formData.phone, formData.bloodGroup, formData.dateOfJoining, formData.fathersName, formData.fathersOccupation, formData.countryOfOrigin
+    // formData.nationality, formData.emergencyContactName, formData.emergencyContactNumber, formData.emergencyContactRelation,
+    //     formData.spouseName, formData.physicallyChallenged, formData.education, formData.addressprofType
+
 })
 
 
